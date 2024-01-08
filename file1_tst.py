24c9cf60e603a500c7b43a2cad38df1a6db1149d
Oleksii <alekseev.aleksey@outlook.com>
2024-01-08 22:36:43 +0200

    commit 3 (test)

--- file1_tst.py
+++ file1_tst.py
@@ -1,5 +1,7 @@
-fsbdghdf
-xcgkfmnldsf
-xdgdf
-dsxfsdg
-dkfjeoirgjdfsgdrgerghrthrth
+print('test')
+print(2 ** 100)
+
+import sys
+
+major, minor, micro = sys.version_info[:3]
+print(f"Your Python version is {major}.{minor}.{micro}")
